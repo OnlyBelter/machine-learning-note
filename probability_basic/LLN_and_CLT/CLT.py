f64d23f5da01a7491771ebb8e8075ebee7139124
OnlyBelter <xiongxin20008@126.com>
2017-11-14 22:07:09 +0800

    add CLT

--- probability_basic/LLN_and_CLT/CLT.py
+++ probability_basic/LLN_and_CLT/CLT.py
@@ -1,40 +1,90 @@
-import random
+# -*- coding: utf-8 -*-
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import stats
+
+"""
+Created on Sun Nov 12 08:44:37 2017
+
+@author: Belter
+"""
+
+# p = 0.999
+para = 0.4
+size = [1, 4, 20, 80, 200, 10000]
 
 
-def flip_plot(n, p):
+def sampling2pmf(n, p, m=10000):
     """
-    Assumes min_exp and min_exp positive integers; min_exp < max_exp
-    Plots results of 2**min_exp to 2**max_exp coin flips
-    抛硬币的次数为2的min_exp次方到2的max_exp次方
-    一共进行了 2**max_exp - 2**min_exp 轮实验，每轮实验抛硬币次数逐渐增加
+    n: sample size for each experiment
+    m: how many times do you do experiment, fix in 10000
+    p: parameter for distribution
     """
+    ber_dist = stats.bernoulli(p)  # 定义一个伯努利分布
+    sum_of_samples = []
+    for i in range(m):
+        samples = ber_dist.rvs(size=n)
+        sum_of_samples.append(np.sum(samples))
+    val, cnt = np.unique(sum_of_samples, return_counts=True)
+    pmf = cnt / len(sum_of_samples)
+    return val, pmf
 
-    delta_p = []  # delta p for each person's result
-    x_axis = []  # 每个人抛硬币的次数，其长度表示试验人数
-    # X_mean = n * p  # 理论均值
-    for i in range(int(np.sqrt(n))):
-        x_axis.append(n)
-    for numFlips in x_axis:
-        num_heads = 0  # 初始化，硬币正面朝上的计数为0
-        for n in range(numFlips):
-            if random.random() < p:  # random.random()从[0, 1)随机的取出一个数
-                num_heads += 1  # 当随机取出的数小于p时，正面朝上的计数加1
-        num_tails = numFlips - num_heads  # 得到本次试验中反面朝上的次数
-        delta_p.append(num_heads/float(n) - p)  # 每个人的估计值与真实值之间的误差
-    # plt.title('Heads/Tails Ratios')
-    # plt.xlabel('Number of Flips')
-    # plt.ylabel('Heads/Tails')
-    # plt.plot(x_axis, ratios)
-    # plt.hlines(1, 0, x_axis[-1], linestyles='dashed', colors='r')
-    # plt.show()
-    return sum(delta_p)  # 误差和
-
-result = []
-for j in range(10000):
-    result.append(flip_plot(10000, 0.5))
-plt.hist(result, bins=100)
-plt.show()
+# 下面是利用matplotlib画图
+plt.figure(1)
+# plot bernoulli distribution, n = 1
+plt.subplot(321)  # 两行一列，第一个子图
+sample_result = sampling2pmf(n=size[0], p=para)
+# print(sample_result)
+plt.vlines(sample_result[0], 0, sample_result[1],
+           colors='g', linestyles='-', lw=3)
+plt.ylabel('Probability')
+plt.title('PMF of bernoulli dist. (n={})'.format(size[0]))
 
 
+plt.subplot(322)
+sample_result = sampling2pmf(n=size[1], p=para)
+# print(sample_result)
+plt.vlines(sample_result[0], 0, sample_result[1],
+           colors='g', linestyles='-', lw=3)
+plt.ylabel('Probability')
+plt.title('Sum of bernoulli dist. (n={})'.format(size[1]))
+
+
+plt.subplot(323)
+sample_result = sampling2pmf(n=size[2], p=para)
+# print(sample_result)
+plt.vlines(sample_result[0], 0, sample_result[1],
+           colors='g', linestyles='-', lw=3)
+plt.ylabel('Probability')
+plt.title('Sum of bernoulli dist. (n={})'.format(size[2]))
+
+
+plt.subplot(324)
+sample_result = sampling2pmf(n=size[3], p=para)
+# print(sample_result)
+plt.vlines(sample_result[0], 0, sample_result[1],
+           colors='g', linestyles='-', lw=3)
+plt.ylabel('Probability')
+plt.title('Sum of bernoulli dist. (n={})'.format(size[3]))
+
+
+plt.subplot(325)
+sample_result = sampling2pmf(n=size[4], p=para)
+# print(sample_result)
+plt.vlines(sample_result[0], 0, sample_result[1],
+           colors='g', linestyles='-', lw=3)
+plt.ylabel('Probability')
+plt.title('Sum of bernoulli dist. (n={})'.format(size[4]))
+
+
+plt.subplot(326)
+sample_result = sampling2pmf(n=size[5], p=para)
+# print(sample_result)
+plt.vlines(sample_result[0], 0, sample_result[1],
+           colors='g', linestyles='-', lw=3)
+plt.ylabel('Probability')
+plt.title('Sum of bernoulli dist. (n={})'.format(size[5]))
+
+plt.tight_layout()
+# plt.show()
+plt.savefig('sum_of_ber_dist.png', dpi=200)
